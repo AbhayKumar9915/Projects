a7487ba650e63b241832c46ca2159cb50b06971a
ABHAY <ABHAY@10.20.16.122>
2020-01-25 14:54:07 +0530

    Changed

--- InterviewQuestionsTallySolutions/Test02/BrokenLinks.py
+++ InterviewQuestionsTallySolutions/Test02/BrokenLinks.py
@@ -8,6 +8,9 @@ driver.maximize_window()
 driver.get("http://the-internet.herokuapp.com/broken_images")
 time.sleep(2)
 
-driver.find_element_by_xpath("//div[@id='content']//img[1]").click()
+
+Links = driver.find_elements_by_tag_name("img")
+for i in Links:
+    print(i)
 
 driver.close()
